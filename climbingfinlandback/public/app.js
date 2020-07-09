6d284fe6354e003bac619b732d0b026a6cecd4ff
MiikaM <42460214+MiikaM@users.noreply.github.com>
2020-07-09 11:25:14 +0300

    firebase db harjottelua

--- climbingfinlandback/public/app.js
+++ climbingfinlandback/public/app.js
@@ -1,20 +1,25 @@
 document.addEventListener('DOMContentLoaded', event => {
 
   const app = firebase.app()
+
+  const db = firebase.firestore()
+
+  const myPost = db.collection('posts').doc('firstpost')
+  
+  myPost.onSnapshot(doc => {
+
+    const data = doc.data()
+    document.querySelector('#title').innerHTML = data.title
+  })
+  console.log({myPost})
+  
   console.log(app)
+
+
 })
 
-const googleLogin = async () => {
-  const provider = new firebase.auth.GoogleAuthProvider()
-  try {
-    const result = await firebase.auth().signInWithPopup(provider)
-    console.log({result})
-
-    const user = result.user
-    document.write(`Hello ${user.displayName}`)
-    console.log({user})
-  
-  } catch (e) {
-    console.log(e)
-  }
+const updatePost = (e) => {
+  const db = firebase.firestore()
+  const myPost = db.collection('posts').doc('firstpost')
+  myPost.update({title: e.target.value})
 }
