7c5b733d4a5004cb19850a9d9169796ee135ad04
MiikaM <42460214+MiikaM@users.noreply.github.com>
2020-07-09 11:53:32 +0300

    firebase basics done

--- climbingfinlandback/public/app.js
+++ climbingfinlandback/public/app.js
@@ -5,21 +5,42 @@ document.addEventListener('DOMContentLoaded', event => {
   const db = firebase.firestore()
 
   const myPost = db.collection('posts').doc('firstpost')
-  
+
   myPost.onSnapshot(doc => {
 
     const data = doc.data()
     document.querySelector('#title').innerHTML = data.title
   })
-  console.log({myPost})
-  
+  console.log({ myPost })
+
   console.log(app)
 
 
 })
 
+const uploadFile = async (files) => {
+  const storageRef = firebase.storage().ref()
+  const horseRef = storageRef.child('Horse.jpg')
+
+  const file = files.item(0)
+
+  const task = horseRef.put(file)
+
+  console.log({ task })
+  try {
+    const downloadURL = await task.snapshot.ref.getDownloadURL()
+    console.log("File available at", downloadURL);
+    const url = downloadURL;
+    document.querySelector("#imgUpload").setAttribute("src", url);
+  } catch (e) {
+    console.log({e})
+  }
+
+
+}
+
 const updatePost = (e) => {
   const db = firebase.firestore()
   const myPost = db.collection('posts').doc('firstpost')
-  myPost.update({title: e.target.value})
+  myPost.update({ title: e.target.value })
 }
