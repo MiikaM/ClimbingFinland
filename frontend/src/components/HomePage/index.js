a50f0e9fa643f34f42f4b51d6f95db7de22a4231
MiikaM <42460214+MiikaM@users.noreply.github.com>
2020-08-28 14:16:41 +0300

    Sivujen hifi versio vallmiiks ja html alustamista

---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const HomePage = (params) => {
+
+  return (
+    <div>
+
+      <div>
+        <ul>
+          <li>
+            Favourites
+        </li>
+          <li>
+            Log in
+        </li>
+        </ul>
+        
+        <img src='' alt='' />
+        <img src='' alt='' />
+
+        <img src='' alt='' />
+        <input></input>
+
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
