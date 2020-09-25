b847f3a813a6645dc8137d2a2e04ff71e15ebfe8
MiikaM <42460214+MiikaM@users.noreply.github.com>
2020-09-25 14:27:31 +0300

    Revert "joo"
    This reverts commit 1ed18f07aac61a0177dc0a62d0c4a0a84f8a2985.

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import firebase from 'firebase'
 import '../../scss/homepage.scss'
 import LogoBlue from '../../images/logo-blue.svg'
@@ -10,15 +10,14 @@ import Modal from '../Modal'
 import { useDispatch, useSelector } from 'react-redux'
 import DropDownMenu from '../NavHeader/dropDownMenu'
 import '../../scss/dropdown.scss'
+import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 import AvatarSVG from '../../images/avatar.svg'
 import { getUser } from '../../reducers/loginReducer'
-import { useSpring } from 'react-spring'
 
 
 
 const HomePage = (params) => {
   const dispatch = useDispatch()
-  // const fadeIn = useSpring()
   const [dropdown, setDropdown] = useState(false)
   const [isOpen, setIsOpen] = useState(false)
   const user = useSelector(state => state.session)
@@ -42,6 +41,8 @@ const HomePage = (params) => {
   console.log({ user })
 
   return (
+    
+
     <div>
       <section className="hero-home">
         <div className="wrapper">
@@ -64,6 +65,9 @@ const HomePage = (params) => {
               </ul>
             </nav>
           </div>
+
+
+
           <div className="hero-content-home">
 
             <img src={LogoBlue} alt='Top logo' className='top-logo' />
@@ -73,7 +77,6 @@ const HomePage = (params) => {
           </div>
         </div>
       </section>
-
       <Modal open={isOpen} onClose={() => setIsOpen(false)}>
         <Login close={() => setIsOpen(false)} />
       </Modal>
@@ -88,6 +91,7 @@ const HomePage = (params) => {
           <Places />
         </div>
       </section>
+
 
       <Footer />
     </div>
--- frontend/src/components/Places/info.js
+++ frontend/src/components/Places/info.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react'
-import { ParallaxLayer, } from 'react-spring/renderprops-addons'
+import {
+  ListSubheader,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Collapse,
+  Link
+} from '@material-ui/core'
 
+import {
+  ExpandLess,
+  ExpandMore,
+  CheckCircleOutlineOutlined,
+  HighlightOffOutlined
+} from '@material-ui/icons'
 import Prices from './prices'
 import OpenHours from './open_hours'
 
@@ -9,7 +23,7 @@ import Open from './open'
 
 
 
-const Info = ({ place  }) => {
+const Info = ({ place }) => {
   const [showOpenHours, setShowOpenHours] = useState(false)
   const [showPrices, setShowPrices] = useState(false)
   const pricing = !place.prices ?
@@ -32,23 +46,21 @@ const Info = ({ place  }) => {
     setShowPrices(!showPrices)
   }
 
-
   return (
-
-      <li className="place-wrapper">
-        <div className="place-card">
-          <a href={`/gym/${place.name} `}>
-            <img src={place.image} alt={`${place.name} sali`} className="place" />
-          </a>
-          <div className="place-text-wrapper">
-            <h2 className="place-header">{place.name}</h2>
-            <blockquote className="place-description">
-              {place.description}
-            </blockquote>
-            <button className="readmore-button" ><a href={`/gym/${place.name} `}> Read more</a></button>
-          </div>
+    <li className="place-wrapper">
+      <div className="place-card">
+        <a href={`/gym/${place.name} `}>
+          <img src={place.image} alt={`${place.name} sali`} className="place" />
+        </a>
+        <div className="place-text-wrapper">
+          <h2 className="place-header">{place.name}</h2>
+          <blockquote className="place-description">
+            {place.description}
+          </blockquote>
+          <button className="readmore-button" ><a href={`/gym/${place.name} `}> Read more</a></button>
         </div>
-      </li>
+      </div>
+    </li>
   )
 }
 
